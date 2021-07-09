f89cf7fb3ed80f78280b4adbeb1dd34403095b75
Maxgen cocos <sshirin058@gmail.com>
2021-07-09 10:56:18 +0530

    stock market

--- app/src/main/java/com/example/liveproject/MainActivity2.kt
+++ app/src/main/java/com/example/liveproject/MainActivity2.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.cardview.widget.CardView
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import java.nio.channels.AsynchronousFileChannel.open
@@ -17,10 +18,12 @@ class MainActivity2 : AppCompatActivity() {
     lateinit var drawerLayout: DrawerLayout
     lateinit var toggle: ActionBarDrawerToggle
     lateinit var toolbar:Toolbar
-    val name= arrayOf<String>("Intraday", "Short Term","Long Term", "Details", "Contest", "Share")
-    val image=arrayOf<Int>(R.drawable.ic_baseline_business_center_24,R.drawable.ic_baseline_timer_24,
-    R.drawable.ic_baseline_bar_chart_24,R.drawable.ic_baseline_details_24,R.drawable.ic_baseline_category_24,
-    R.drawable.ic_baseline_share_24)
+    lateinit var cardView1: CardView
+    lateinit var cardView2: CardView
+    lateinit var cardView3: CardView
+    lateinit var cardView4: CardView
+    lateinit var cardView5: CardView
+    lateinit var cardView6: CardView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,32 +37,37 @@ class MainActivity2 : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toggle.isDrawerIndicatorEnabled=true
-//
-//        imageView1.setOnClickListener{
-//            val intent= Intent(this,MainActivity3::class.java)
-//         startActivity(intent)
-//        }
-//        imageView2.setOnClickListener{
-//            val intent= Intent(this,MainActivity3::class.java)
-//            startActivity(intent)
-//        }
-//        imageView3.setOnClickListener{
-//            val intent= Intent(this,MainActivity3::class.java)
-//            startActivity(intent)
-//        }
-//        imageView4.setOnClickListener{
-//            val intent= Intent(this,MainActivity3::class.java)
-//            startActivity(intent)
-//        }
-//        imageView5.setOnClickListener{
-//            val intent= Intent(this,MainActivity3::class.java)
-//            startActivity(intent)
-//        }
-//        imageView6.setOnClickListener{
-//            val intent= Intent(this,MainActivity3::class.java)
-//            startActivity(intent)
-//        }
-//
+        cardView1=findViewById(R.id.card1)
+        cardView2=findViewById(R.id.card2)
+        cardView3=findViewById(R.id.card3)
+        cardView4=findViewById(R.id.card4)
+        cardView5=findViewById(R.id.card5)
+        cardView6=findViewById(R.id.card6)
+        cardView1.setOnClickListener{
+            val intent= Intent(this,MainActivity3::class.java)
+         startActivity(intent)
+        }
+        cardView2.setOnClickListener{
+            val intent= Intent(this,MainActivity3::class.java)
+            startActivity(intent)
+        }
+        cardView3.setOnClickListener{
+            val intent= Intent(this,MainActivity3::class.java)
+            startActivity(intent)
+        }
+        cardView4.setOnClickListener{
+            val intent= Intent(this,MainActivity3::class.java)
+            startActivity(intent)
+        }
+        cardView5.setOnClickListener{
+            val intent= Intent(this,MainActivity3::class.java)
+            startActivity(intent)
+        }
+        cardView6.setOnClickListener{
+            val intent= Intent(this,MainActivity3::class.java)
+            startActivity(intent)
+        }
+
 
 
 
--- app/src/main/java/com/example/liveproject/MainActivity3.kt
+++ app/src/main/java/com/example/liveproject/MainActivity3.kt
@@ -3,17 +3,31 @@ package com.example.liveproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity3 : AppCompatActivity() {
+    lateinit var navigationView: NavigationView
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var toolbar: Toolbar
     lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
         bottomNavigationView=findViewById(R.id.navigation)
+        navigationView=findViewById(R.id.navigationview)
+        drawerLayout=findViewById(R.id.dra)
+        toolbar=findViewById(R.id.tool)
+        setSupportActionBar(toolbar)
+        toggle= ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.isDrawerIndicatorEnabled=true
 
 
-//        val fragment = FirstFragment()
-//        supportFragmentManager.beginTransaction().replace(R.id.frame, fragment).commit()
     }
 }
