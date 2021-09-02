bb8d29f97f78457527743619032414a5d7f40964
azhar <shaikhnoorish8@gmail.com>
2021-09-02 10:16:48 +0530

    stock market

--- app/src/main/java/com/example/liveproject/BlankFragment5.kt
+++ app/src/main/java/com/example/liveproject/BlankFragment5.kt
@@ -1,10 +1,12 @@
 package com.example.liveproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
@@ -33,6 +35,11 @@ class BlankFragment5 : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         var textView:TextView=view.findViewById(R.id.contesttext)
+        var button:Button=view.findViewById(R.id.loginbutton)
+        button.setOnClickListener {
+            val intent=Intent(context,MainActivity6::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
